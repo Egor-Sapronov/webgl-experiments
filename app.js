6d212b0fbd077879fe2958536dffe19856c8c633
Egor <sapronov.egor@gmail.com>
2014-05-23 14:54:52 +0700

    Update.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var container;
 
-var camera, scene, renderer;
+var camera, scene, renderer, controls;
 
 var cube, plane;
 
@@ -21,26 +21,162 @@ function init() {
     document.body.appendChild(container);
 
     camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.y = 150;
+    camera.position.y = 500;
     camera.position.z = 500;
+
+    controls = new THREE.TrackballControls(camera);
+
+    controls.rotateSpeed = 1.0;
+    controls.zoomSpeed = 1.2;
+    controls.panSpeed = 0.8;
+
+    controls.noZoom = false;
+    controls.noPan = false;
+
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.3;
+
+    controls.keys = [ 65, 83, 68 ];
+
+    controls.addEventListener('change', render);
 
     scene = new THREE.Scene();
 
-    // Cube
 
-    var geometry = new THREE.BoxGeometry(200, 200, 200);
+    function frontWall(x) {
+        var geometry = new THREE.BoxGeometry(1, 10, 40);
+        for (var i = 0; i < geometry.faces.length; i += 2) {
+            var hex = Math.random() * 0xffffff;
+            geometry.faces[ i ].color.setHex(hex);
+            geometry.faces[ i + 1 ].color.setHex(hex);
+        }
+        var material = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors, overdraw: 0.5});
+        cube = new THREE.Mesh(geometry, material);
+        cube.position.y = 0;
+        cube.position.x = x;
+        scene.add(cube);
 
-    for (var i = 0; i < geometry.faces.length; i += 2) {
-        var hex = Math.random() * 0xffffff;
-        geometry.faces[ i ].color.setHex(hex);
-        geometry.faces[ i + 1 ].color.setHex(hex);
+        var geometry = new THREE.BoxGeometry(1, 10, 40);
+        for (var i = 0; i < geometry.faces.length; i += 2) {
+            var hex = Math.random() * 0xffffff;
+            geometry.faces[ i ].color.setHex(hex);
+            geometry.faces[ i + 1 ].color.setHex(hex);
+        }
+        var material = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors, overdraw: 0.5});
+        cube = new THREE.Mesh(geometry, material);
+        cube.position.y = 20;
+        cube.position.x = x;
+        scene.add(cube);
+
+        var geometry = new THREE.BoxGeometry(1, 10, 12);
+        for (var i = 0; i < geometry.faces.length; i += 2) {
+            var hex = Math.random() * 0xffffff;
+            geometry.faces[ i ].color.setHex(hex);
+            geometry.faces[ i + 1 ].color.setHex(hex);
+        }
+        var material = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors, overdraw: 0.5});
+        cube = new THREE.Mesh(geometry, material);
+        cube.position.y = 10;
+        cube.position.z = 14;
+        cube.position.x = x;
+        scene.add(cube);
+
+        var geometry = new THREE.BoxGeometry(1, 10, 12);
+        for (var i = 0; i < geometry.faces.length; i += 2) {
+            var hex = Math.random() * 0xffffff;
+            geometry.faces[ i ].color.setHex(hex);
+            geometry.faces[ i + 1 ].color.setHex(hex);
+        }
+        var material = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors, overdraw: 0.5});
+        cube = new THREE.Mesh(geometry, material);
+        cube.position.y = 10;
+        cube.position.z = -14;
+        cube.position.x = x;
+        scene.add(cube);
     }
 
-    var material = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors, overdraw: 0.5});
+    function wall() {
+        var geometry = new THREE.BoxGeometry(1, 30, 50);
+        geometry.applyMatrix(new THREE.Matrix4().makeRotationY(-Math.PI / 2));
+        for (var i = 0; i < geometry.faces.length; i += 2) {
+            var hex = Math.random() * 0xffffff;
+            geometry.faces[ i ].color.setHex(hex);
+            geometry.faces[ i + 1 ].color.setHex(hex);
+        }
+        var material = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors, overdraw: 0.5});
+        cube = new THREE.Mesh(geometry, material);
+        cube.position.y = 10;
+        cube.position.x = 25;
+        cube.position.z = -20;
+        scene.add(cube);
+    }
 
-    cube = new THREE.Mesh(geometry, material);
-    cube.position.y = 150;
-    scene.add(cube);
+    function wallWithDoor() {
+        var geometry = new THREE.BoxGeometry(1, 10, 50);
+        geometry.applyMatrix(new THREE.Matrix4().makeRotationY(-Math.PI / 2));
+        for (var i = 0; i < geometry.faces.length; i += 2) {
+            var hex = Math.random() * 0xffffff;
+            geometry.faces[ i ].color.setHex(hex);
+            geometry.faces[ i + 1 ].color.setHex(hex);
+        }
+        var material = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors, overdraw: 0.5});
+        cube = new THREE.Mesh(geometry, material);
+        cube.position.y = 20;
+        cube.position.x = 25;
+        cube.position.z = 20;
+        scene.add(cube);
+
+        var geometry = new THREE.BoxGeometry(1, 20, 30);
+        geometry.applyMatrix(new THREE.Matrix4().makeRotationY(-Math.PI / 2));
+        for (var i = 0; i < geometry.faces.length; i += 2) {
+            var hex = Math.random() * 0xffffff;
+            geometry.faces[ i ].color.setHex(hex);
+            geometry.faces[ i + 1 ].color.setHex(hex);
+        }
+        var material = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors, overdraw: 0.5});
+        cube = new THREE.Mesh(geometry, material);
+        cube.position.y = 5;
+        cube.position.x = 15;
+        cube.position.z = 20;
+        scene.add(cube);
+
+        var geometry = new THREE.BoxGeometry(1, 20, 10);
+        geometry.applyMatrix(new THREE.Matrix4().makeRotationY(-Math.PI / 2));
+        for (var i = 0; i < geometry.faces.length; i += 2) {
+            var hex = Math.random() * 0xffffff;
+            geometry.faces[ i ].color.setHex(hex);
+            geometry.faces[ i + 1 ].color.setHex(hex);
+        }
+        var material = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors, overdraw: 0.5});
+        cube = new THREE.Mesh(geometry, material);
+        cube.position.y = 5;
+        cube.position.x = 45;
+        cube.position.z = 20;
+        scene.add(cube);
+    }
+
+    function floor(){
+        var geometry = new THREE.BoxGeometry(1, 40, 50);
+        geometry.applyMatrix(new THREE.Matrix4().makeRotationY(-Math.PI / 2));
+        geometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
+        for (var i = 0; i < geometry.faces.length; i += 2) {
+            var hex = Math.random() * 0xffffff;
+            geometry.faces[ i ].color.setHex(hex);
+            geometry.faces[ i + 1 ].color.setHex(hex);
+        }
+        var material = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors, overdraw: 0.5});
+        cube = new THREE.Mesh(geometry, material);
+        cube.position.y = -5;
+        cube.position.x = 25;
+        cube.position.z = 0;
+        scene.add(cube);
+    }
+
+    frontWall(0);
+    frontWall(50);
+    wall();
+    wallWithDoor();
+    floor();
 
     renderer = new THREE.CanvasRenderer();
     renderer.setClearColor(0xf0f0f0);
@@ -116,11 +252,12 @@ function onDocumentTouchMove(event) {
 
 function animate() {
     requestAnimationFrame(animate);
-
+    controls.update();
     render();
 }
 
 function render() {
-    cube.rotation.y += (targetRotation - cube.rotation.y) * 0.05;
+//    cube.rotation.y += (targetRotation - cube.rotation.y) * 0.05;
+
     renderer.render(scene, camera);
 }
