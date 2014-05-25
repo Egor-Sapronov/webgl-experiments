440127de457b13f43881195389e0f9b0f7bea7c5
Egor <sapronov.egor@gmail.com>
2014-05-25 11:28:32 +0700

    Add floor.

--- house.js
+++ house.js
@@ -2,184 +2,49 @@ var container;
 
 var camera, scene, renderer, controls;
 
-var shape, plane;
-
-var targetRotation = 0;
-var targetRotationOnMouseDown = 0;
-
-var mouseX = 0;
-var mouseXOnMouseDown = 0;
-
-var windowHalfX = window.innerHeight / 2;
-var windowHalfY = window.innerWidth / 2;
-
 init();
 animate();
 
 function init() {
+    scene = new THREE.Scene();
+
     container = document.createElement('div');
     document.body.appendChild(container);
 
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.y = 150;
-    camera.position.z = 500;
-
-    scene = new THREE.Scene();
-
-    // Back wall
-    var geometry = new THREE.PlaneGeometry(700,700);
-    geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
-    for (var i = 0; i < geometry.faces.length; i += 2) {
-        var hex = Math.random() * 0xffffff;
-        geometry.faces[ i ].color.setHex(hex);
-        geometry.faces[ i + 1 ].color.setHex(hex);
-    }
-    var material = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors, overdraw: 0.5});
-    shape = new THREE.Mesh(geometry,material);
-    shape.position.z=0;
-    scene.add(shape);
-
-    // Floor
-    var geometry = new THREE.PlaneGeometry(600,1000);
-    for (var i = 0; i < geometry.faces.length; i += 2) {
-        var hex = Math.random() * 0xffffff;
-        geometry.faces[ i ].color.setHex(hex);
-        geometry.faces[ i + 1 ].color.setHex(hex);
-    }
-    var material = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors, overdraw: 0.5});
-    shape = new THREE.Mesh(geometry,material);
-    shape.position.z=-230;
-    scene.add(shape);
-
-    // Left side
-    var geometry = new THREE.PlaneGeometry(700,800);
-    geometry.applyMatrix( new THREE.Matrix4().makeRotationY( - Math.PI / 2 ) );
-    for (var i = 0; i < geometry.faces.length; i += 2) {
-        var hex = Math.random() * 0xffffff;
-        geometry.faces[ i ].color.setHex(hex);
-        geometry.faces[ i + 1 ].color.setHex(hex);
-    }
-    var material = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors, overdraw: 0.5});
-    shape = new THREE.Mesh(geometry,material);
-    shape.position.x=400;
-    shape.position.y=250;
-    shape.position.z=-120;
-    scene.add(shape);
-
-
-    var geometry = new THREE.BoxGeometry(100,100,100);
-    geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
-    for (var i = 0; i < geometry.faces.length; i += 2) {
-        var hex = Math.random() * 0xffffff;
-        geometry.faces[ i ].color.setHex(hex);
-        geometry.faces[ i + 1 ].color.setHex(hex);
-    }
-    var material = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors, overdraw: 0.5});
-    shape = new THREE.Mesh(geometry,material);
-    shape.position.y=120;
-    shape.position.z=150;
-    scene.add(shape);
-
+    // CAMERA
+    var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
+    var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
+    camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+    camera.position.set(0,150,400);
+    camera.lookAt(scene.position);
 
     renderer = new THREE.CanvasRenderer();
     renderer.setClearColor(0xffffff);
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 
     container.appendChild(renderer.domElement);
 
-    document.addEventListener( 'mousedown', onDocumentMouseDown, false );
-    document.addEventListener( 'touchstart', onDocumentTouchStart, false );
-    document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-    //
-
-    window.addEventListener( 'resize', onWindowResize, false );
+    // Floor
+    var floorTexture = new THREE.ImageUtils.loadTexture('checkerboard.jpg');
+    floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
+    floorTexture.repeat.set( 10, 10 );
+    var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
+    var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+    var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    floor.position.y = -0.5;
+    floor.rotation.x = Math.PI / 2;
+    scene.add(floor);
 }
 
-function onWindowResize() {
-
-    windowHalfX = window.innerWidth / 2;
-    windowHalfY = window.innerHeight / 2;
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
-
-//
-
-function onDocumentMouseDown( event ) {
-
-    event.preventDefault();
-
-    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.addEventListener( 'mouseup', onDocumentMouseUp, false );
-    document.addEventListener( 'mouseout', onDocumentMouseOut, false );
-
-    mouseXOnMouseDown = event.clientX - windowHalfX;
-    targetRotationOnMouseDown = targetRotation;
-
-}
-
-function onDocumentMouseMove( event ) {
-
-    mouseX = event.clientX - windowHalfX;
-
-    targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
-
-}
-
-function onDocumentMouseUp( event ) {
-
-    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
-    document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
-
-}
-
-function onDocumentMouseOut( event ) {
-
-    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
-    document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
-
-}
-
-function onDocumentTouchStart( event ) {
-
-    if ( event.touches.length === 1 ) {
-
-        event.preventDefault();
-
-        mouseXOnMouseDown = event.touches[ 0 ].pageX - windowHalfX;
-        targetRotationOnMouseDown = targetRotation;
-
-    }
-
-}
-
-function onDocumentTouchMove( event ) {
-
-    if ( event.touches.length === 1 ) {
-
-        event.preventDefault();
-
-        mouseX = event.touches[ 0 ].pageX - windowHalfX;
-        targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.05;
-
-    }
-
-}
 
 function animate() {
     requestAnimationFrame(animate);
-
+    controls.update();
     render();
 }
 
-function render(){
-    shape.rotation.y += ( targetRotation - shape.rotation.y ) * 0.05;
+function render() {
     renderer.render(scene, camera);
 }
