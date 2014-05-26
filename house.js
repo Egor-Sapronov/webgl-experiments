7b7102c3bd397a61547420029813c36f1efb28bc
Egor <sapronov.egor@gmail.com>
2014-05-26 15:07:32 +0700

    Delete old files.

--- house.js
+++ house.js
@@ -1,12 +1,12 @@
 var container;
 
-var camera, scene, renderer, controls;
+var camera, scene, renderer, controls, light, dirLight, hemiLight;
 
 var group;
 
 var targetRotation = 0;
 var targetRotationOnMouseDown = 0;
-
+var pointLight;
 var mouseX = 0;
 var mouseXOnMouseDown = 0;
 
@@ -16,17 +16,23 @@ var windowHalfY = window.innerHeight / 2;
 var brickTexture = new THREE.ImageUtils.loadTexture('bricks.jpg');
 brickTexture.wrapS = brickTexture.wrapT = THREE.RepeatWrapping;
 brickTexture.repeat.set(2, 2);
-var brickMaterial = new THREE.MeshBasicMaterial({map: brickTexture, side: THREE.DoubleSide});
+var brickMaterial = new THREE.MeshLambertMaterial({map: brickTexture, side: THREE.DoubleSide, ambient: 0x555555, specular: 0xffffff, shininess: 50, shading: THREE.SmoothShading });
 
-var wallTexture=new THREE.ImageUtils.loadTexture('wall.jpg');
-wallTexture.wrapS=wallTexture.wrapT=THREE.RepeatWrapping;
-wallTexture.repeat.set(2,2);
-var wallMaterial=new THREE.MeshBasicMaterial({map:wallTexture,side:THREE.BackSide});
+var wallTexture = new THREE.ImageUtils.loadTexture('wall.jpg');
+wallTexture.wrapS = wallTexture.wrapT = THREE.RepeatWrapping;
+wallTexture.repeat.set(2, 2);
+var wallMaterial = new THREE.MeshLambertMaterial({map: wallTexture, side: THREE.BackSide, shading: THREE.FlatShading});
 
 var roofTexture = new THREE.ImageUtils.loadTexture('roof.jpg');
 roofTexture.wrapS = roofTexture.wrapT = THREE.RepeatWrapping;
 roofTexture.repeat.set(2, 2);
-var roofMaterial = new THREE.MeshBasicMaterial({map: roofTexture, side: THREE.DoubleSide});
+var roofMaterial = new THREE.MeshLambertMaterial({map: roofTexture, side: THREE.DoubleSide, shading: THREE.FlatShading});
+
+var woodTexture = new THREE.ImageUtils.loadTexture('wood.jpg');
+woodTexture.wrapS = woodTexture.wrapT = THREE.RepeatWrapping;
+woodTexture.repeat.set(2, 2);
+var woodMaterial = new THREE.MeshLambertMaterial({map: woodTexture, side: THREE.DoubleSide, shading: THREE.FlatShading});
+
 
 init();
 animate();
@@ -39,13 +45,12 @@ function init() {
 
     // CAMERA
     var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
-    var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 1000;
+    var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 10000;
     camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-    camera.position.set(0, 150, 200);
+    camera.position.set(0, 50, 200);
     camera.lookAt(scene.position);
 
     renderer = new THREE.CanvasRenderer();
-    renderer.setClearColor(0xffffff);
     renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 
     container.appendChild(renderer.domElement);
@@ -56,7 +61,7 @@ function init() {
     var floorTexture = new THREE.ImageUtils.loadTexture('checkerboard.jpg');
     floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
     floorTexture.repeat.set(10, 10);
-    var floorMaterial = new THREE.MeshBasicMaterial({ map: floorTexture, side: THREE.DoubleSide });
+    var floorMaterial = new THREE.MeshPhongMaterial({ map: floorTexture, side: THREE.DoubleSide, ambient: 0xffffff, color: 0xffffff, specular: 0x050505   });
     var floorGeometry = new THREE.PlaneGeometry(100, 100, 10, 10);
     var floor = new THREE.Mesh(floorGeometry, floorMaterial);
     floor.position.y = 0;
@@ -66,14 +71,57 @@ function init() {
     group = new THREE.Object3D();
     scene.add(group);
 
+    light = new THREE.PointLight(0xff0040, 2, 50);
+    scene.add(light);
+    var sphere = new THREE.SphereGeometry(0.5, 16, 8);
+
+    var l1 = new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0xff0040 }));
+    l1.position = light.position;
+    scene.add(l1);
+
     createWalls();
     createRoof();
+    createTable();
 
     document.addEventListener('mousedown', onDocumentMouseDown, false);
     document.addEventListener('touchstart', onDocumentTouchStart, false);
     document.addEventListener('touchmove', onDocumentTouchMove, false);
     document.addEventListener('keydown', onDocumentKeyDown, false);
     document.addEventListener('keyup', onDocumentKeyUp, false);
+}
+
+function createTable() {
+    var geometry=new THREE.BoxGeometry(7,0.2,5);
+    var mesh=new THREE.Mesh(geometry,woodMaterial);
+    mesh.position.y=3;
+    mesh.position.z=2.5;
+    group.add(mesh);
+
+    var geometry=new THREE.BoxGeometry(0.2,3,0.2);
+    var mesh=new THREE.Mesh(geometry,woodMaterial);
+    mesh.position.y=1.5;
+    mesh.position.z=1;
+    mesh.position.x=2;
+    group.add(mesh);
+    var geometry=new THREE.BoxGeometry(0.2,3,0.2);
+    var mesh=new THREE.Mesh(geometry,woodMaterial);
+    mesh.position.y=1.5;
+    mesh.position.z=1;
+    mesh.position.x=-2;
+    group.add(mesh);
+
+    var geometry=new THREE.BoxGeometry(0.2,3,0.2);
+    var mesh=new THREE.Mesh(geometry,woodMaterial);
+    mesh.position.y=1.5;
+    mesh.position.z=4;
+    mesh.position.x=2;
+    group.add(mesh);
+    var geometry=new THREE.BoxGeometry(0.2,3,0.2);
+    var mesh=new THREE.Mesh(geometry,woodMaterial);
+    mesh.position.y=1.5;
+    mesh.position.z=4;
+    mesh.position.x=-2;
+    group.add(mesh);
 }
 
 function onDocumentKeyDown(event) {
@@ -150,8 +198,8 @@ function onDocumentTouchMove(event) {
 }
 
 function createWalls() {
-    var material = new THREE.MeshBasicMaterial({ color: 0x0000ff, side: THREE.DoubleSide,opacity:0.3 });
-    var material2 = new THREE.MeshBasicMaterial({ color: 0x0000ff, side: THREE.DoubleSide,opacity:0.0 });
+    var material = new THREE.MeshBasicMaterial({ color: 0x0000ff, side: THREE.DoubleSide, opacity: 0.3 });
+    var material2 = new THREE.MeshBasicMaterial({ color: 0x0000ff, side: THREE.DoubleSide, opacity: 0.0 });
     var materials = [
         material,
         brickMaterial,
@@ -163,30 +211,31 @@ function createWalls() {
     var geometry = new THREE.PlaneGeometry(40, 10, 10, 10);
     var l = geometry.faces.length;
     for (var i = 0; i < l; i++) {
-        geometry.faces[i].materialIndex=1;
+        geometry.faces[i].materialIndex = 1;
     }
-    geometry.faces[44].materialIndex=0;
-    geometry.faces[45].materialIndex=0;
-    geometry.faces[64].materialIndex=0;
-    geometry.faces[65].materialIndex=0;
-    geometry.faces[84].materialIndex=0;
-    geometry.faces[85].materialIndex=0;
-    geometry.faces[104].materialIndex=0;
-    geometry.faces[105].materialIndex=0;
-    geometry.faces[124].materialIndex=0;
-    geometry.faces[125].materialIndex=0;
-    geometry.faces[54].materialIndex=0;
-    geometry.faces[55].materialIndex=0;
-    geometry.faces[74].materialIndex=0;
-    geometry.faces[75].materialIndex=0;
-    geometry.faces[94].materialIndex=0;
-    geometry.faces[95].materialIndex=0;
-    geometry.faces[114].materialIndex=0;
-    geometry.faces[115].materialIndex=0;
-    geometry.faces[134].materialIndex=0;
-    geometry.faces[135].materialIndex=0;
+    geometry.faces[44].materialIndex = 0;
+    geometry.faces[45].materialIndex = 0;
+    geometry.faces[64].materialIndex = 0;
+    geometry.faces[65].materialIndex = 0;
+    geometry.faces[84].materialIndex = 0;
+    geometry.faces[85].materialIndex = 0;
+    geometry.faces[104].materialIndex = 0;
+    geometry.faces[105].materialIndex = 0;
+    geometry.faces[124].materialIndex = 0;
+    geometry.faces[125].materialIndex = 0;
+    geometry.faces[54].materialIndex = 0;
+    geometry.faces[55].materialIndex = 0;
+    geometry.faces[74].materialIndex = 0;
+    geometry.faces[75].materialIndex = 0;
+    geometry.faces[94].materialIndex = 0;
+    geometry.faces[95].materialIndex = 0;
+    geometry.faces[114].materialIndex = 0;
+    geometry.faces[115].materialIndex = 0;
+    geometry.faces[134].materialIndex = 0;
+    geometry.faces[135].materialIndex = 0;
     var mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
     mesh.position.y = 5;
+    mesh.position.z = 10;
     group.add(mesh);
 
     // Front wall with door
@@ -194,47 +243,47 @@ function createWalls() {
     geometry.applyMatrix(new THREE.Matrix4().makeRotationY(-Math.PI / 2));
     var l = geometry.faces.length;
     for (var i = 0; i < l; i++) {
-        geometry.faces[i].materialIndex=1;
+        geometry.faces[i].materialIndex = 1;
     }
-    geometry.faces[82].materialIndex=3;
-    geometry.faces[83].materialIndex=3;
-    geometry.faces[102].materialIndex=3;
-    geometry.faces[103].materialIndex=3;
-    geometry.faces[122].materialIndex=3;
-    geometry.faces[123].materialIndex=3;
-    geometry.faces[142].materialIndex=3;
-    geometry.faces[143].materialIndex=3;
-    geometry.faces[162].materialIndex=3;
-    geometry.faces[163].materialIndex=3;
-    geometry.faces[182].materialIndex=3;
-    geometry.faces[183].materialIndex=3;
-    geometry.faces[84].materialIndex=3;
-    geometry.faces[85].materialIndex=3;
-    geometry.faces[104].materialIndex=3;
-    geometry.faces[105].materialIndex=3;
-    geometry.faces[124].materialIndex=3;
-    geometry.faces[125].materialIndex=3;
-    geometry.faces[144].materialIndex=3;
-    geometry.faces[145].materialIndex=3;
-    geometry.faces[164].materialIndex=3;
-    geometry.faces[165].materialIndex=3;
-    geometry.faces[184].materialIndex=3;
-    geometry.faces[185].materialIndex=3;
+    geometry.faces[82].materialIndex = 3;
+    geometry.faces[83].materialIndex = 3;
+    geometry.faces[102].materialIndex = 3;
+    geometry.faces[103].materialIndex = 3;
+    geometry.faces[122].materialIndex = 3;
+    geometry.faces[123].materialIndex = 3;
+    geometry.faces[142].materialIndex = 3;
+    geometry.faces[143].materialIndex = 3;
+    geometry.faces[162].materialIndex = 3;
+    geometry.faces[163].materialIndex = 3;
+    geometry.faces[182].materialIndex = 3;
+    geometry.faces[183].materialIndex = 3;
+    geometry.faces[84].materialIndex = 3;
+    geometry.faces[85].materialIndex = 3;
+    geometry.faces[104].materialIndex = 3;
+    geometry.faces[105].materialIndex = 3;
+    geometry.faces[124].materialIndex = 3;
+    geometry.faces[125].materialIndex = 3;
+    geometry.faces[144].materialIndex = 3;
+    geometry.faces[145].materialIndex = 3;
+    geometry.faces[164].materialIndex = 3;
+    geometry.faces[165].materialIndex = 3;
+    geometry.faces[184].materialIndex = 3;
+    geometry.faces[185].materialIndex = 3;
     var mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
     mesh.position.y = 5;
-    mesh.position.z=5;
-    mesh.position.x=20;
+    mesh.position.z = 5;
+    mesh.position.x = 20;
     group.add(mesh);
 
     // Wall
     var geometry = new THREE.PlaneGeometry(40, 10, 10, 10);
     var l = geometry.faces.length;
     for (var i = 0; i < l; i++) {
-        geometry.faces[i].materialIndex=1;
+        geometry.faces[i].materialIndex = 1;
     }
     var mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
     mesh.position.y = 5;
-    mesh.position.z=10;
+//    mesh.position.z=10;
     group.add(mesh);
 
     // Side wall with two windows
@@ -242,53 +291,53 @@ function createWalls() {
     geometry.applyMatrix(new THREE.Matrix4().makeRotationY(-Math.PI / 2));
     var l = geometry.faces.length;
     for (var i = 0; i < l; i++) {
-        geometry.faces[i].materialIndex=1;
+        geometry.faces[i].materialIndex = 1;
     }
-    geometry.faces[42].materialIndex=0;
-    geometry.faces[43].materialIndex=0;
-    geometry.faces[62].materialIndex=0;
-    geometry.faces[63].materialIndex=0;
-    geometry.faces[82].materialIndex=0;
-    geometry.faces[83].materialIndex=0;
-    geometry.faces[102].materialIndex=0;
-    geometry.faces[103].materialIndex=0;
-    geometry.faces[122].materialIndex=0;
-    geometry.faces[123].materialIndex=0;
-    geometry.faces[44].materialIndex=0;
-    geometry.faces[45].materialIndex=0;
-    geometry.faces[64].materialIndex=0;
-    geometry.faces[65].materialIndex=0;
-    geometry.faces[84].materialIndex=0;
-    geometry.faces[85].materialIndex=0;
-    geometry.faces[104].materialIndex=0;
-    geometry.faces[105].materialIndex=0;
-    geometry.faces[124].materialIndex=0;
-    geometry.faces[125].materialIndex=0;
+    geometry.faces[42].materialIndex = 0;
+    geometry.faces[43].materialIndex = 0;
+    geometry.faces[62].materialIndex = 0;
+    geometry.faces[63].materialIndex = 0;
+    geometry.faces[82].materialIndex = 0;
+    geometry.faces[83].materialIndex = 0;
+    geometry.faces[102].materialIndex = 0;
+    geometry.faces[103].materialIndex = 0;
+    geometry.faces[122].materialIndex = 0;
+    geometry.faces[123].materialIndex = 0;
+    geometry.faces[44].materialIndex = 0;
+    geometry.faces[45].materialIndex = 0;
+    geometry.faces[64].materialIndex = 0;
+    geometry.faces[65].materialIndex = 0;
+    geometry.faces[84].materialIndex = 0;
+    geometry.faces[85].materialIndex = 0;
+    geometry.faces[104].materialIndex = 0;
+    geometry.faces[105].materialIndex = 0;
+    geometry.faces[124].materialIndex = 0;
+    geometry.faces[125].materialIndex = 0;
 
-    geometry.faces[56].materialIndex=0;
-    geometry.faces[57].materialIndex=0;
-    geometry.faces[54].materialIndex=0;
-    geometry.faces[55].materialIndex=0;
-    geometry.faces[76].materialIndex=0;
-    geometry.faces[77].materialIndex=0;
-    geometry.faces[74].materialIndex=0;
-    geometry.faces[75].materialIndex=0;
-    geometry.faces[96].materialIndex=0;
-    geometry.faces[97].materialIndex=0;
-    geometry.faces[94].materialIndex=0;
-    geometry.faces[95].materialIndex=0;
-    geometry.faces[116].materialIndex=0;
-    geometry.faces[117].materialIndex=0;
-    geometry.faces[114].materialIndex=0;
-    geometry.faces[115].materialIndex=0;
-    geometry.faces[136].materialIndex=0;
-    geometry.faces[137].materialIndex=0;
-    geometry.faces[134].materialIndex=0;
-    geometry.faces[135].materialIndex=0;
+    geometry.faces[56].materialIndex = 0;
+    geometry.faces[57].materialIndex = 0;
+    geometry.faces[54].materialIndex = 0;
+    geometry.faces[55].materialIndex = 0;
+    geometry.faces[76].materialIndex = 0;
+    geometry.faces[77].materialIndex = 0;
+    geometry.faces[74].materialIndex = 0;
+    geometry.faces[75].materialIndex = 0;
+    geometry.faces[96].materialIndex = 0;
+    geometry.faces[97].materialIndex = 0;
+    geometry.faces[94].materialIndex = 0;
+    geometry.faces[95].materialIndex = 0;
+    geometry.faces[116].materialIndex = 0;
+    geometry.faces[117].materialIndex = 0;
+    geometry.faces[114].materialIndex = 0;
+    geometry.faces[115].materialIndex = 0;
+    geometry.faces[136].materialIndex = 0;
+    geometry.faces[137].materialIndex = 0;
+    geometry.faces[134].materialIndex = 0;
+    geometry.faces[135].materialIndex = 0;
     var mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
     mesh.position.y = 5;
-    mesh.position.z=5;
-    mesh.position.x=-20;
+    mesh.position.z = 5;
+    mesh.position.x = -20;
     group.add(mesh);
 }
 
@@ -297,14 +346,14 @@ function createRoof() {
     geometry.applyMatrix(new THREE.Matrix4().makeRotationX(Math.PI / 4));
     var mesh = new THREE.Mesh(geometry, roofMaterial);
     mesh.position.z = 2;
-    mesh.position.y = 13.75;
+    mesh.position.y = 12.2;
     group.add(mesh);
 
     var geometry = new THREE.PlaneGeometry(40, 8.5, 10, 10);
     geometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 4));
     var mesh = new THREE.Mesh(geometry, roofMaterial);
     mesh.position.z = 8;
-    mesh.position.y = 13.75;
+    mesh.position.y = 12.2;
     group.add(mesh);
 
     var geometry = new THREE.Geometry();
@@ -319,14 +368,14 @@ function createRoof() {
     geometry.faces.push(new THREE.Face3(0, 1, 2));
     geometry.computeFaceNormals();
     geometry.applyMatrix(new THREE.Matrix4().makeRotationY(-Math.PI / 2));
-    var mesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial({map: brickTexture, side: THREE.DoubleSide}));
-    mesh.position.y = 11.5;
+    var mesh = new THREE.Mesh(geometry, brickMaterial);
+    mesh.position.y = 10;
     mesh.position.z = 0;
     mesh.position.x = 20;
     group.add(mesh);
 
-    var mesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial({map: brickTexture, side: THREE.DoubleSide}));
-    mesh.position.y = 11.5;
+    var mesh = new THREE.Mesh(geometry, brickMaterial);
+    mesh.position.y = 10;
     mesh.position.z = 0;
     mesh.position.x = -20;
     group.add(mesh);
@@ -345,11 +394,15 @@ function render() {
             group.position.x += ( targetRotation - group.position.y ) * 0.05;
         else {
             if (group.position.x >= 30)
-                group.position.x = 29;
+                group.position.x = 29.999999;
             else
-                group.position.x = -29;
+                group.position.x = -29.999999;
         }
     }
+    var time = Date.now() * 0.0005;
+    light.position.x = Math.sin(time * 0.7) * 30;
+    light.position.y = Math.cos(time * 0.5) * 40;
+    light.position.z = Math.cos(time * 0.3) * 30;
 
     renderer.render(scene, camera);
 }
